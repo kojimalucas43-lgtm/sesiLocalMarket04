a5c5fab8b16d446d3d85b24efef6baf966a314e7
Kojima Lucas <kojimalucas43@gmail.com>
2026-06-19 11:05:54 -0300

    Add files via upload

--- src/login/script.js
+++ src/login/script.js
@@ -2,9 +2,36 @@ function login(){
     var nome = $("#nome").val()
     var senha = $("#senha").val()
 
-    if(nome && senha & nome === "admin" && senha === "43"){
-
+    if(nome && senha && nome === "admin" && senha === "43"){
+        const user = {
+            name: nome,
+            dataEntrada: new Date(),
+            id:Math.floor(Math.random() * 100000)
+        }
+        localStorage.setItem("usuario", JSON.stringify(user))
+        window.location.href = "../lojas"
     }else{
-        
+        document.getElementById("error-modal").style.display = "flex"
+        document.getElementById("nome").style.border = "3px solid red"
+        document.getElementById("senha").style.border = "3px solid red"
+    }
+}
+
+function fecharError(){
+    document.getElementById("error-modal").style.display = "none"
+    document.getElementById("nome").style.border = "2px solid black"
+    document.getElementById("senha").style.border = "2px solid black"
+}
+
+function showPassword(){
+    var inputSenha = document.querySelector("#senha")
+    var img_eye = document.querySelector("#eye")
+
+    if(inputSenha.getAttribute("type") === "password"){
+        inputSenha.setAttribute("type", "text")
+        img_eye.setAttribute("src", "../../public/visivel.png")
+    }else{
+        inputSenha.setAttribute("type", "password")
+        img_eye.setAttribute("src", "../../public/olho.png")
     }
 }
---
+++ src/lojas/script.js
@@ -0,0 +1,47 @@
+let produtos
+
+window.onload = function (){
+    var storedUser = localStorage.getItem("usuario")
+    var user = JSON.parse(storedUser)
+    var dataEntrada = new Date(user.dataEntrada)
+
+    var dataFormaatda = dataEntrada.toLocaleString("pt-BR",{
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour:"numeric",
+        minute:"numeric"
+    })
+
+    document.getElementById('user').textContent = user.name
+    document.getElementById('perfil').textContent = dataFormaatda
+    document.getElementById('idPerfil').textContent = user.id
+    
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetch("../dados/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        produtos = data
+
+        const produtosContainer = document.getElementById("produtos-coteiner")
+
+        produtos.forEach((produto, index) => {
+            const card = document.createElement("div");
+            card.innerHTML = `<div class="card" style="width: 18rem;">
+                                    <img src="${produto.imagem}" class="card-img-top" alt="${produto.desc}">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${produto.desc}</h5>
+                                        <p class="card-text">Salário${produto.sal}</p>
+                                        <a href="#" class="btn btn-primary">
+                                        Go somewhere
+                                        </a>
+                                    </div>
+                                </div>
+                            `
+                            produtosContainer.appendChild(card)
+        })
+    }).catch((error)=> console.log("Erro ao carregar dados", error)) 
+
+})
